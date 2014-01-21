ecb301275fe07acd158370fd16badfb91957dd9e
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2014-01-21 13:36:05 -0500

    nginx_request_headers.php

---
+++ nginx_request_headers.php
@@ -0,0 +1,14 @@
+// apache_request_headers replicement for nginx
+if (!function_exists('apache_request_headers')) { 
+        function apache_request_headers() { 
+            foreach($_SERVER as $key=>$value) { 
+                if (substr($key,0,5)=="HTTP_") { 
+                    $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5))))); 
+                    $out[$key]=$value; 
+                }else{
+                    $out[$key]=$value; 
+		}
+            } 
+            return $out; 
+        } 
+}
